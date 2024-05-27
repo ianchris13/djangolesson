085187db07fa9720ff36e99e52e4f8402d0c4f21
Ian Chris <ianchrism3@gmail.com>
2024-05-28 02:13:23 +0800

    Latest lesson

--- crudapp/models.py
+++ crudapp/models.py
@@ -7,9 +7,12 @@ class Gender(models.Model):
     gender = models.CharField(max_length=55, blank=False)
     date_created = models.DateTimeField(auto_now_add=True)
     date_updated = models.DateTimeField(auto_now=True)
-
+    
     class Meta:
         db_table = 'genders'
+
+    def __str__(self):
+        return self.gender
 
 class User(models.Model):
     user_id = models.BigAutoField(primary_key=True, blank=False)
@@ -17,7 +20,7 @@ class User(models.Model):
     middle_name = models.CharField(max_length=55, blank=True)
     last_name = models.CharField(max_length=55, blank=False)
     age = models.IntegerField(blank=False)
-    birt_date = models.DateField(blank=False)
+    birth_date = models.DateField(blank=False)
     gender = models.ForeignKey(Gender, on_delete=models.CASCADE)
     username = models.CharField(max_length=55, blank=False)
     password = models.CharField(max_length=255, blank=False)
